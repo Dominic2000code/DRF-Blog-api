1677169c3c7cf6171c289ef84754306fd4efc5bd
Dominic Bluawofogbe <bluawofogbedominic@gmail.com>
2022-11-30 14:07:20 +0000

    initial commit

---
+++ posts/views.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from .models import Post
+from .serializers import PostSerializer, UserSerializer
+from .permissions import IsAuthorOrReadOnly
+
+# Create your views here.
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly,)
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Django Blog api with DRF
+
+---
+Blog api with user authentication and authorization
+
+---
